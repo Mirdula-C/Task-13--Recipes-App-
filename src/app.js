e38dc22c7d07e2e6f5ab409431dd2d2a6b924931
Mirdula-C <mirdula311204@gmail.com>
2025-03-25 15:24:49 +0530

    Add root route for Recipe API

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Recipe API!");
+});
+
 app.use("/api/recipes", recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
